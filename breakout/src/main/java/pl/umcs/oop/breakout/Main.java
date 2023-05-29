c5ae388396c60eabcb9828f740e2f45a88f3bdf2
Krzysztof Dmitruk <kdmitruk@gmail.com>
2023-05-29 16:18:16 +0200

    breakout

---
+++ breakout/src/main/java/pl/umcs/oop/breakout/Main.java
@@ -0,0 +1,32 @@
+package pl.umcs.oop.breakout;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        GameCanvas gameCanvas = new GameCanvas();
+
+
+        GridPane pane = new GridPane();
+        pane.add(gameCanvas, 0, 0);
+        stage.setScene(new Scene(pane));
+
+
+        gameCanvas.initialize();
+        gameCanvas.draw();
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
